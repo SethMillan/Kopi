931868fdeafdbcde05768aeb93824d9cefa89f35
EfrenMV <efrenvillanueva148@gmail.com>
2025-03-19 02:33:36 -0600

    Update[login js]
    Ahora el .text_login vuelve visible a .derecha lo cual hace que podamos pasar de login a crear usuario y viceversa sin ningun problema

--- assets/js/login_script.js
+++ assets/js/login_script.js
@@ -15,3 +15,20 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const signupElement = document.querySelector(".text_logIn"); //Click en .text_Signup
+
+    signupElement.addEventListener("click", function () {
+
+        // Desaparecer el login 
+        document.querySelectorAll(".derecha").forEach(element => {
+            element.style.display = "flex"; // Oculta los elementos
+        });
+
+        //Mostrar el nuevo usuario
+        document.querySelectorAll(".nuew_user").forEach(element => {
+            element.style.display = "none";
+        });
+    });
+});
