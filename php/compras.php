016de3a1f123d5a00411106fc562dcf6684d56e8
EfrenMV <efrenvillanueva148@gmail.com>
2025-04-24 15:40:58 -0600

    Update[README.md]
    Se actualizaron las tareas

--- php/compras.php
+++ php/compras.php
@@ -7,7 +7,45 @@ try {
     exit;
 }
 
-function a ($datos, $conn) {}
-function b ($datos, $conn) {}
+
+
+function verCarrito ( $conn) {
+    session_start();
+    
+    if (!isset($_SESSION['cliente_id'])) {
+        return ['success' => false, 'message' => 'No hay sesión iniciada'];
+    }
+
+    $clienteid = $_SESSION['cliente_id'];
+
+    $result = pg_query_params($conn, "SELECT producto_id, cantidad  FROM carrito WHERE cliente_id = $1", [$clienteid]);
+
+    if (!$result) {
+        return ['success' => false, 'message' => 'carrito basio'];
+    }
+
+    $carrito = [];
+    while ($row = pg_fetch_assoc($result)) {
+        $carrito[] = $row;
+    }
+
+    return ['success' => true, 'carrito' => $carrito];
+}
+
+function AgregarCarrito ($datos, $conn) {
+    session_start();
+    
+    if (!isset($_SESSION['cliente_id'])) {
+        return ['success' => false, 'message' => 'No hay sesión iniciada'];
+    }
+
+    $clienteid = $_SESSION['cliente_id'];
+    $producto_id = $datos['producto_id'];
+
+
+    $result = pg_query_params($conn, "INSERT INTO carrito ( cliente_id, producto_id, cantidad) VALUES ($1, $2, $3)",
+    [$clienteid , $producto_id, 1]); 
+
+}
 
 ?>
